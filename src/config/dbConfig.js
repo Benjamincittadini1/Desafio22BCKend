f1f52d022bdd72fb04b1d2d216321e2a064c7f27
Benjamincittadini1 <86275653+Benjamincittadini1@users.noreply.github.com>
2022-06-30 22:32:12 -0300

    Initial commit

---
+++ src/config/dbConfig.js
@@ -0,0 +1,11 @@
+ const config = {
+    mongoDb: {
+        url: 'mongodb+srv://MarcosLabra:34851809a@coderback.pfiz1.mongodb.net/desClass22?retryWrites=true&w=majority',
+        options: {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        }
+    }
+}
+
+export default config;
